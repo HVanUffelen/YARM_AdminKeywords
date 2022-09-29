02c57b712eeeab6d8de7a3ad8d78beb839bd4522
vanuffh7 <herbert.van-uffelen@univie.ac.at>
2022-09-29 12:53:36 +0200

    start package

--- src/AdminKeywordsServiceProvider.php
+++ src/AdminKeywordsServiceProvider.php
@@ -12,7 +12,7 @@ class AdminKeywordsServiceProvider extends ServiceProvider{
         $this->loadRoutesFrom(__DIR__ . '/routes/web.php');
         $this->loadViewsFrom(__DIR__ . '/views','adminkeywords');
         $this->loadMigrationsFrom(__DIR__.'/database/migrations');
-        $this->mergeConfigFrom(__DIR__ . '/config/Adminkeywords.php','adminkeywords');
+        $this->mergeConfigFrom(__DIR__ . '/config/AdminKeywords.php','adminkeywords');
         $this->publishes([
             //__DIR__ . '/config/bookshelf.php' => config_path('bookshelf.php'),
             //__DIR__ . '/views' => resource_path('views/vendor/adminkeywords'),
